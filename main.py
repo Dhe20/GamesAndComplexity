627cb3a9fbedcb7ba7e924845edcf24b1415e993
Ivan Shalashilin <ivanshalashilin@dyn3153-155.wlan.ic.ac.uk>
2022-11-07 11:59:45 +0000

    to let me merge

--- main.py
+++ main.py
@@ -7,14 +7,14 @@ import time;
 x = Iterator(20,100, Ternary = True)
 # x.CreateEmptyRowCol(RoworCol="C", WhichRowCol= 0) # "R" or "C", then which one of the rows/cols
 # x.CreateEmptyRowCol(RoworCol="R", WhichRowCol= 0)
-x.CreateEmptyDiagonal(UporDown="D")
-x.CreateEmptyDiagonal(UporDown="U")
-x.VisualiseGrid()
+# x.CreateEmptyDiagonal(UporDown="D")
+# x.CreateEmptyDiagonal(UporDown="U")
+# x.VisualiseGrid()
 
-x.Run(SaveData=True, KillOrBeKilledAndLearn = True)
+# x.Run(SaveData=True, KillOrBeKilledAndLearn = True)
 
-Eval = Metrics(x.GetFileName())
-Eval.AnimateEvolution(50)
+Eval = Metrics('pkl/100_20_uafvq.pkl')
+Eval.AnimateEvolution(500)
 Eval.PlotSimilarity()
 Eval.PlotRPSAmount()
 
